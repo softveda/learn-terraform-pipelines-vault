12b5f16fdb6054b87f4f10676caff3eaf546282d
Pratik Khasnabis <pratik.khasnabis@outlook.com>
2021-06-04 00:11:28 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
     organization = "infrastructure-pipelines-workshop"
 
     workspaces {
-      name = "john-d-vault"
+      name = "pratik-k-vault"
     }
   }
   required_providers {
